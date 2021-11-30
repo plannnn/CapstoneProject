cf72e421d300db667c1e8ff1b52c8e76ae09b7ef
Sang Putu Yoga Pramana <yogapramana8@gmail.com>
2021-11-30 22:17:34 +0800

    Menambahkan folder static yang berisikan file assets, css dan js

---
+++ static/js/scripts.js
@@ -0,0 +1,44 @@
+const welcome = document.getElementById('welcome');
+const start = document.getElementById('start');
+const predict = document.getElementById('predict');
+const result = document.getElementById('result');
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+});
+
+start.addEventListener("click", function () {
+    predict.classList.remove("d-none")
+    result.classList.remove("d-none")
+    welcome.classList.add("d-none")
+    start.classList.add("d-none")
+});
